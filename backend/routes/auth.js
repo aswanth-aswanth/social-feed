7fd8188b40907d98b3a7f406cffed3a399483769
aswanth-aswanth <aswanthndl@gmail.com>
2024-12-19 13:55:46 +0530

    feat: add dummy route for uptime

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,18 +1,29 @@
-const router = require('express').Router();
-const passport = require('passport');
+const router = require("express").Router();
+const passport = require("passport");
 const {
   register,
   login,
   googleCallback,
   resetPassword,
-  forgotPassword
-} = require('../controllers/authController');
+  forgotPassword,
+} = require("../controllers/authController");
 
-router.post('/register', register);
-router.post('/login', login);
-router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
-router.get('/google/callback', passport.authenticate('google', { session: false }), googleCallback);
-router.post('/reset-password/:token', resetPassword);
-router.post('/forgot-password', forgotPassword);
+router.post("/register", register);
+router.post("/login", login);
+router.get(
+  "/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+router.get(
+  "/google/callback",
+  passport.authenticate("google", { session: false }),
+  googleCallback
+);
+router.post("/reset-password/:token", resetPassword);
+router.post("/forgot-password", forgotPassword);
+router.get("/log", (req, res) => {
+  console.log("dummy route for uptime ping");
+  res.json({ message: "Hello World" });
+});
 
-module.exports = router; 
+module.exports = router;
